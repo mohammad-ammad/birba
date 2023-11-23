53b49cac68a645c3a32d77b1858072283b7bd7df
kakugb <karamat.4101@gmail.com>
2023-11-24 01:06:40 +0500

    Second commit

--- app/floor-map/page.js
+++ app/floor-map/page.js
@@ -5,7 +5,7 @@ const bannerStyles = {
     color: "white",
     textAlign: "center",
     padding: "50px 10px",
-    width: "100%",
+    width: "430px",
 };
 export default function FloorMap() {
   return (
@@ -13,91 +13,93 @@ export default function FloorMap() {
       <div style={bannerStyles}>
         <div className="text-3xl font-bold">Floor Map</div>
       </div>
-      <div className=" md:w-[100%]  md:flex md:flex-row md:justify-around sm:flex sm:flex-col sm:justify-around sm:mx-auto  ">
-          <div className="sm:w-[376px] sm:h-[176px] sm:top-[192px] sm:left-[29px] sm:p-[10px] sm:gap-[5] sm:mt-8 sm:bg-[#FFFFFF]   sm:drop-shadow-2xl sm:shadow-[#000000] sm:border sm:border-3 sm:flex sm:flex-col sm:mx-auto
-           md:w-[257px] md:h-[184px]   md:mt-[100px] md:left-[96px] md:p-[5px] md:gap-[5px] md:bg-[#FFFFFF]  md:drop-shadow-2xl md:shadow-[#000000] md:border md:border-3 md:flex md:flex-col">
-            <div className=" sm:w-[341px] sm:h-[32px] sm:flex sm:justify-around  md:w-[257px] md:h-[32px] md:top-[282px] md:left-[96px] md:p-[10px] md:gap-[5px] md:flex md:justify-around ">
-               <p className="sm:w-[130px] sm:h-[20px] sm:text-[#AE0243] sm:text-[12px] sm:leading-[20px]  md:w-[130px] md:h-[20px] md:font-[Poppins] md:font-[600] md:text-[13px] md:text-[#AE0243] sm:font-bold">Floor Map Sections</p>           
+      <div className=" md:w-[430px] flex flex-col justify-around   md:flex md:flex-row md:justify-around ">
+          <div className="w-[300px] h-[184px] p-[10px] px-14 mx-auto border border-inherit  mt-7 md:w-[257px] md:h-[184px]   md:mt-[100px] md:left-[96px] md:p-[5px] md:gap-[5px] md:bg-[#FFFFFF]  md:drop-shadow-2xl md:shadow-[#000000] md:border md:border-3 md:flex md:flex-col">
+            <div className="  md:w-[257px] md:h-[32px] md:top-[282px] md:left-[96px] md:p-[10px] md:gap-[5px] md:flex md:justify-around ">
+               <p className="flex justify-around p-[10px]  text-[#AE0243] font-bold md:w-[130px] md:h-[20px] md:font-[Poppins] md:font-[600] md:text-[13px] md:text-[#AE0243] sm:font-bold">Floor Map Sections</p>           
             </div>
 
             
-            <div className="sm:w-[341px] sm:h-[36px] sm:gap-[10px] sm:flex sm:flex-row sm:justify-between md:w-[237px] md:h-[36px] md:gap-[10px]  md:flex md:flex-row md:justify-between">
-                   <p className="sm:w-[275px] sm:h-[20px] md:w-[175px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">Area One</p>
-                   <p className="sm:w-[130px] sm:h-[20px]sm:w-[65px] sm:h-[20px] md:w-[65px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">G1 - G5</p>
+            <div className="flex flex-row justify-between md:w-[237px] md:h-[36px] md:gap-[10px]  md:flex md:flex-row md:justify-between">
+                   <p className=" md:w-[175px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">Area One</p>
+                   <p className=" md:w-[65px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">G1 - G5</p>
             </div>
-            <div className="sm:w-[341px] sm:h-[36px] sm:gap-[10px] sm:flex sm:flex-row sm:justify-between md:w-[237px] md:h-[36px] md:gap-[5px] md:flex md:flex-row md:justify-between">
-                   <p className="sm:w-[275px] sm:h-[20px]  md:w-[175px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">Area Two</p>
-                   <p className="sm:w-[130px] sm:h-[20px] md:w-[65px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">A1 - A5</p>
+            <div className="flex flex-row justify-between md:w-[237px] md:h-[36px] md:gap-[5px] md:flex md:flex-row md:justify-between">
+                   <p className="  md:w-[175px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">Area Two</p>
+                   <p className=" md:w-[65px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">A1 - A5</p>
             </div>
-            <div className="sm:w-[341px] sm:h-[36px] sm:gap-[10px] sm:flex sm:flex-row sm:justify-between md:w-[237px] md:h-[36px] md:gap-[5px] md:flex md:flex-row md:justify-between">
-                   <p className="sm:w-[275px] sm:h-[20px]  md:w-[175px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">Area Two</p>
-                   <p className="sm:w-[130px] sm:h-[20px] md:w-[65px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">A1 - A5</p>
+            <div className="flex flex-row justify-between md:w-[237px] md:h-[36px] md:gap-[5px] md:flex md:flex-row md:justify-between">
+                   <p className=" md:w-[175px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">Area Two</p>
+                   <p className=" md:w-[65px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">A1 - A5</p>
             </div>
-            <div className="sm:w-[341px] sm:h-[36px] sm:gap-[10px] sm:flex sm:flex-row sm:justify-between md:w-[237px] md:h-[36px] md:gap-[5px] md:flex md:flex-row md:justify-between">
-                   <p className="sm:w-[275px] sm:h-[20px]  md:w-[175px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">Area Two</p>
-                   <p className="sm:w-[130px] sm:h-[20px] md:w-[65px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">A1 - A5</p>
+            <div className="flex flex-row justify-between md:w-[237px] md:h-[36px] md:gap-[5px] md:flex md:flex-row md:justify-between">
+                   <p className=" md:w-[175px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">Area Two</p>
+                   <p className=" md:w-[65px] md:h-[20px] md:text-[13px] md:font-[500] md:leading-[20px]">A1 - A5</p>
             </div>
           </div>
 
-      <div className="sm:w-[385px] sm:h-[361px] sm:top-[408px] sm:left-[23px] sm:p-[10px] sm:flex sm:flex-wrap sm:justify-between sm:mx-auto md:w-[913px] md:h-[617px] md:mt-[100px]  md:left-[435] md:top-[282px] md:p-[10px] md:bg-[#FFFFFF] md:flex md:flex-wrap md:justify-between">
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#40D09A]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#40D09A]"></div>
-            <div className="sm:w-[120px] sm-h-[26px] md:w-[280px] md:h-[56px] bg-[#40D09A]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FF6D9880]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FF6D9880]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FF6D9880]"></div>
+      <div className="w-[300px] h-[361px] mx-auto mt-7  flex flex-wrap justify-between md:w-[913px] md:h-[617px] md:mt-[100px]  md:left-[435] md:top-[282px] md:p-[10px] md:bg-[#FFFFFF] md:flex md:flex-wrap md:justify-between">
+            <div className="w-[35px] h-[26px] md:w-[112px] md:h-[56px] bg-[#40D09A]"></div>
+            <div className="w-[35px] h-[26px] md:w-[112px] md:h-[56px] bg-[#40D09A]"></div>
+            <div className="w-[105px] h-[26px] md:w-[280px] md:h-[56px] bg-[#40D09A]"></div>
+            <div className="w-[35px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FF6D9880]"></div>
+            <div className="w-[35px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FF6D9880]"></div>
+            <div className="w-[35px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FF6D9880]"></div>
 
 
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FF266080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FF2660]"></div>
 
 
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>          
-             <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>        
-             <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FF266080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>          
+             <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#00BAF080]"></div>        
+             <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FF2660]"></div>
 
-            <div className="sm:w-[49px] sm-h-[35px] md:w-[112px] md:h-[77px] bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm-h-[35px] md:w-[112px] md:h-[77px] bg-[rgba(0,186,240,0.5)]"></div>
-            <div className="sm:w-[48px] sm-h-[35px] md:w-[112px] md:h-[77px] bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm-h-[36px] md:w-[112px] md:h-[77px] bg-[rgba(255,163,0,0.5)]"></div>
-            <div className="sm:w-[48px] sm-h-[36px] md:w-[112px] md:h-[77px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[36px] md:w-[112px] md:h-[77px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FF266080]"></div>
-
-
-
-            <div className="sm:w-[48px] sm-h-[36px] md:w-[112px] md:h-[77px] bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm-h-[36px] md:w-[112px] md:h-[77px] bg-[rgba(0,186,240,0.5)]"></div>
-            <div className="sm:w-[49px] sm-h-[36px] md:w-[112px] md:h-[77px] bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm-h-[36px] md:w-[112px] md:h-[77px] bg-[rgba(255,163,0,0.5)]"></div>
-            <div className="sm:w-[48px] sm-h-[36px] md:w-[112px] md:h-[77px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[36px] md:w-[112px] md:h-[77px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FF266080]"></div>
-
-
-            <div className="sm:w-[48px] sm-h-[45px] md:w-[112px] md:h-[77px] bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm-h-[45px] md:w-[112px] md:h-[77px] bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm-h-[45px] md:w-[112px] md:h-[77px] bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[49px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm-h-[26px] md:w-[112px] md:h-[56px] bg-[#FF266080]"></div>
+            <div className="w-[39px] h-[35px] md:w-[112px] md:h-[77px] bg-[#9AD15D]"></div>
+            <div className="w-[38px] h-[35px] md:w-[112px] md:h-[77px] bg-[#9AD15D]"></div>
+            <div className="w-[38px] h-[35px] md:w-[112px] md:h-[77px] bg-[#9AD15D]"></div>
+            <div className="w-[38px] h-[36px] md:w-[112px] md:h-[77px] bg-[#FFA300]"></div>
+            <div className="w-[38px] h-[36px] md:w-[112px] md:h-[77px] bg-[#FFA300]"></div>
+            <div className="w-[38px] h-[36px] md:w-[112px] md:h-[77px] bg-[#FFA300]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FF2660]"></div>
 
 
 
-            <div className="sm:w-[48px] sm:h-[26px] md:w-[112px] md:h-[56px] md:bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm:h-[26px] md:w-[112px] md:h-[56px] md:bg-[#00BAF080]"></div>
-            <div className="sm:w-[48px] sm:h-[26px] md:w-[112px] md:h-[56px] md:bg-[#FFA30080]"></div>
-            <div className="sm:w-[48px] sm:h-[26px] md:w-[112px] md:h-[56px] md:bg-[#FF266080]"></div>
+            <div className="w-[38px] h-[36px] md:w-[112px] md:h-[77px] bg-[#9AD15D]"></div>
+            <div className="w-[38px] h-[36px] md:w-[112px] md:h-[77px] bg-[#9AD15D]"></div>
+            <div className="w-[39px] h-[36px] md:w-[112px] md:h-[77px] bg-[#9AD15D]"></div>
+            <div className="w-[38px] h-[36px] md:w-[112px] md:h-[77px] bg-[#FFA300]"></div>
+            <div className="w-[38px] h-[36px] md:w-[112px] md:h-[77px] bg-[#FFA300]"></div>
+            <div className="w-[38px] h-[36px] md:w-[112px] md:h-[77px] bg-[#FFA300]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FF2660]"></div>
+
+
+            <div className="w-[38px] h-[45px] md:w-[112px] md:h-[77px] bg-[#00AE9E]"></div>
+            <div className="w-[38px] h-[45px] md:w-[112px] md:h-[77px] bg-[#00AE9E]"></div>
+            <div className="w-[38px] h-[45px] md:w-[112px] md:h-[77px] bg-[#00AE9E]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA300]"></div>
+            <div className="w-[39px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA300]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FFA300]"></div>
+            <div className="w-[38px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FF2660]"></div>
+
+
+            <div className="w-[48px] h-[26px] md:w-[112px] md:h-[77px] bg-[#00AE9E]"></div>
+            <div className="w-[48px] h-[26px] md:w-[112px] md:h-[77px] bg-[#00AE9E]"></div>
+            <div className="w-[48px] h-[26px] md:w-[112px] md:h-[77px] bg-[#FFA300]"></div>
+            <div className="w-[48px] h-[26px] md:w-[112px] md:h-[56px] bg-[#FF2660]"></div>
+
+
+
+
    </div>
  </div>
 </div>
