244509647ee4d957b2899c03e50846b249680e53
kakugb <karamat.4101@gmail.com>
2023-11-24 01:30:20 +0500

    Final comint

--- app/floor-map/page.js
+++ app/floor-map/page.js
@@ -5,7 +5,7 @@ const bannerStyles = {
     color: "white",
     textAlign: "center",
     padding: "50px 10px",
-    width: "430px",
+    width: "100%",
 };
 export default function FloorMap() {
   return (
@@ -13,10 +13,10 @@ export default function FloorMap() {
       <div style={bannerStyles}>
         <div className="text-3xl font-bold">Floor Map</div>
       </div>
-      <div className=" md:w-[430px] flex flex-col justify-around   md:flex md:flex-row md:justify-around ">
-          <div className="w-[300px] h-[184px] p-[10px] px-14 mx-auto border border-inherit  mt-7 md:w-[257px] md:h-[184px]   md:mt-[100px] md:left-[96px] md:p-[5px] md:gap-[5px] md:bg-[#FFFFFF]  md:drop-shadow-2xl md:shadow-[#000000] md:border md:border-3 md:flex md:flex-col">
-            <div className="  md:w-[257px] md:h-[32px] md:top-[282px] md:left-[96px] md:p-[10px] md:gap-[5px] md:flex md:justify-around ">
-               <p className="flex justify-around p-[10px]  text-[#AE0243] font-bold md:w-[130px] md:h-[20px] md:font-[Poppins] md:font-[600] md:text-[13px] md:text-[#AE0243] sm:font-bold">Floor Map Sections</p>           
+      <div className=" md:w-[100%] flex flex-col justify-around  md:flex md:flex-row md:justify-around ">
+          <div className="w-[300px] h-[184px] p-[10px] px-14 mx-auto border border-inherit  mt-7 md:w-[257px] md:h-[184px] md:ml-14  md:mt-[100px] md:left-[96px] md:p-[5px] md:gap-[5px] md:bg-[#FFFFFF]  md:drop-shadow-2xl md:shadow-[#000000] md:border md:border-3 md:flex md:flex-col">
+            <div className="  md:w-[257px] md:h-[32px] md:top-[282px] md:left-[96px]  md:p-[10px] md:gap-[5px] md:flex md:justify-around ">
+               <p className="flex justify-around p-[10px]  text-[#AE0243] font-bold md:w-[130px] md:h-[20px] md:font-[Poppins] md:font-[600] md:text-[13px] md:text-[#AE0243] ">Floor Map Sections</p>           
             </div>
 
             
@@ -38,7 +38,7 @@ export default function FloorMap() {
             </div>
           </div>
 
-      <div className="w-[300px] h-[361px] mx-auto mt-7  flex flex-wrap justify-between md:w-[913px] md:h-[617px] md:mt-[100px]  md:left-[435] md:top-[282px] md:p-[10px] md:bg-[#FFFFFF] md:flex md:flex-wrap md:justify-between">
+      <div className="w-[300px] h-[361px] mx-auto mt-7  flex flex-wrap justify-between md:w-[913px] md:h-[617px] md:mt-[100px] md:p-[10px] md:bg-[#FFFFFF] md:flex md:flex-wrap md:justify-between">
             <div className="w-[35px] h-[26px] md:w-[112px] md:h-[56px] bg-[#40D09A]"></div>
             <div className="w-[35px] h-[26px] md:w-[112px] md:h-[56px] bg-[#40D09A]"></div>
             <div className="w-[105px] h-[26px] md:w-[280px] md:h-[56px] bg-[#40D09A]"></div>
